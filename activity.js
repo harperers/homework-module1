644cd7d2e8763dfac99e479f333c358a12f29208
harperers <max@pan-ami.com>
2024-02-08 15:54:13 -0800

    Small Fix to activity table

--- activity.js
+++ activity.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("td").hover(function () {
-        if ($(this).html() != "Not Available") {
+        if ($(this).html() != "Not Available" && $(this).html() != "Hiking" && $(this).html() != "Kayak" && $(this).html() != "Skydiving" && $(this).html() != "Biking" && $(this).html() != "Camping") {
             $(this).css("cursor", "pointer");
         }
     },
@@ -9,7 +9,7 @@ $(document).ready(function () {
         });
     $(document).click(function (e) {
         var target = $(e.target).closest("td");
-        if (target.html() != "Not Available") {
+        if (target.html() != "Not Available" && target.html() != "Hiking" && target.html() != "Kayak" && target.html() != "Skydiving" && target.html() != "Biking" && target.html() != "Camping") {
             target.toggleClass("highlight");
         }
     });
